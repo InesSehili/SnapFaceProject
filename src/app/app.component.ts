c2a887d4e526971bde688fb2664d0049e1e2f394
InesSehili <inessehili96@gmail.com>
2022-08-09 11:46:50 +0100

    pc moi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,23 +21,16 @@ export class AppComponent implements OnInit{
 
     //this.interval$ = interval(1000);
 
-    this.interval$ = interval(1000).pipe(
+   /* this.interval$ = interval(1000).pipe(
       filter(value => value % 3 === 0),
       map(value => value % 2 === 0 ?
         `Je suis ${value} et je suis pair` :
         `Je suis ${value} et je suis impair`
       ),
       tap(text => this.logger(text))
-    );
-
-
-
-
-
+    );*/
 
   }
-
-
   logger(text: string): void {
     console.log(`Log: ${text}`);
   }
